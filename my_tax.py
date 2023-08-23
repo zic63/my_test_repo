71b6919a0803cabfbdb86a325f453286163ec001
Sergio <zic9226@gmail.com>
2023-08-23 11:55:18 +0200

    My_first_commit

---
+++ my_tax.py
@@ -0,0 +1,21 @@
+names = ['Larry', 'Kehrly', 'Moo', 'Bubby', 'Scanny']
+
+
+
+def num_of_idiots(names_list):
+    example_dict = {1: 'One',
+                  2: 'Two',
+                  3: 'Three',
+                  4: 'Four',
+                  5: 'Five'}
+    
+    return example_dict[len(names_list)]
+message = f'{num_of_idiots(names)} idiots: '
+
+for index, name in enumerate(names, start=1):
+    if index > 1 and index != len(names):
+        message += ', '
+    if index == len(names):
+        message += ' and '
+    message += name
+print(message)
